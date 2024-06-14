9f32b1efed29ed96254de4fcbfce0d1c9d184a93
thaihaiudtp <nguyenmanhthaiha@gmail.com>
2024-06-15 00:31:00 +0700

    add manhinh

---
+++ manhinh/man hinh nha hang/home.js
@@ -0,0 +1,9 @@
+document.getElementById('modal-container').style.display = 'none';
+document.getElementById('btn-open').addEventListener('click', function(){
+    document.getElementById('modal-container').style.display = 'block';
+    
+})
+document.getElementById('btn-close').addEventListener('click', function(){
+    document.getElementById('modal-container').style.display = 'none';
+  
+})
---
+++ manhinh/man hinh nha hang/submit.js
@@ -0,0 +1,3 @@
+document.getElementById('submit').addEventListener('click', function(){
+    document.getElementById('modal-container').style.display = 'none';
+})
