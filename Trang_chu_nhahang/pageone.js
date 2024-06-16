7e75ce4f75e1f1ab0389d5db07e0d1028fa0bfb7
thaihaiudtp <nguyenmanhthaiha@gmail.com>
2024-06-16 23:30:13 +0700

    add TRANGCHUNHAHANG

---
+++ Trang_chu_nhahang/pageone.js
@@ -0,0 +1,9 @@
+document.getElementById('modal-container').style.display = 'none';
+document.getElementById('datban').addEventListener('click', function(){
+    document.getElementById('modal-container').style.display = 'block';
+    
+})
+document.getElementById('btn-close').addEventListener('click', function(){
+    document.getElementById('modal-container').style.display = 'none';
+  
+})
